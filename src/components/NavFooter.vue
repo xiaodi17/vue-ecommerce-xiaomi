55ff7134bef9acf15f0fe56d6beec87f9e4633af
David <xiaodi.david@gmail.com>
2020-01-12 00:48:31 +1000

    NavFooter component

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,8 +1,52 @@
 <template>
-  <div></div>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="" />
+      <p>Mi Mall</p>
+    </div>
+    <div class="footer-link">
+      <a href="">LEARN MORE</a><span>|</span> <a href="">SUPPORT</a><span>|</span> <a href="">ABOUT US</a><span>|</span>
+      <a href="">FOLLOW MI</a>
+    </div>
+    <div class="copyright">Copyright © 2010 - 2020 <span class="domain">Xiaomi</span> All Rights Reserved</div>
+  </div>
 </template>
 <script>
 export default {
   name: 'NavFooter'
 }
 </script>
+<style lang="scss" scoped>
+.footer {
+  height: 234px;
+  border-top: 4px solid #ff6600;
+  background-color: #333333;
+  color: #999999;
+  font-size: 16px;
+  text-align: center;
+  .footer-logo {
+    margin-top: 46px;
+    margin-bottom: 31px;
+    img {
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px;
+    }
+  }
+  .footer-link {
+    a {
+      color: #999999;
+      display: inline-block;
+    }
+    span {
+      margin: 0 10px;
+    }
+    margin-bottom: 13px;
+  }
+  .copyright {
+    .domain {
+      color: #ff6600;
+    }
+  }
+}
+</style>

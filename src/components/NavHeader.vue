30eedfb381b59f20f3738815f36cf802810f9dfb
David <xiaodi.david@gmail.com>
2020-01-12 01:18:48 +1000

    ServiceBar component

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -140,13 +140,12 @@ export default {
       this.axios
         .get('/products', {
           params: {
-            categoryId: '100012'
+            categoryId: '100012',
+            pageSize: 6
           }
         })
         .then(res => {
-          if (res.list.length > 6) {
-            this.phoneList = res.list.slice(0, 6)
-          }
+          this.phoneList = res.list
         })
     },
     goToCart() {
@@ -246,6 +245,7 @@ export default {
             box-shadow: 0px 7px 6px 0px rgba(0, 0, 0, 0.11);
             z-index: 10;
             transition: all 0.5s;
+            background-color: #ffffff;
             .product {
               position: relative;
               float: left;
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <service-bar></service-bar>
+  </div>
 </template>
 <script>
+import ServiceBar from './../components/ServiceBar'
+
 export default {
-  name: 'Index'
+  name: 'Index',
+  components: {
+    ServiceBar
+  }
 }
 </script>

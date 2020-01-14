8603566e737d6fcaf0b3112aa59c27c2abf8cf8e
David <xiaodi.david@gmail.com>
2020-01-14 22:40:28 +1000

    Ads bar

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -52,8 +52,16 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a :href="'/#/product/'+item.id" v-for="(item,index) in adslist" :key="index">
+          <img :src="item.img" />
+        </a>
+      </div>
+      <div class="banner">
+        <a href="'/#/product/30'">
+          <img src="/imgs/banner-1.png" alt />
+        </a>
+      </div>
       <div class="product-box"></div>
     </div>
     <service-bar></service-bar>
@@ -139,6 +147,24 @@ export default {
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
+      ],
+      adslist: [
+        {
+          id: 33,
+          img: '/imgs/ads/ads-1.png'
+        },
+        {
+          id: 48,
+          img: '/imgs/ads/ads-2.jpg'
+        },
+        {
+          id: 45,
+          img: '/imgs/ads/ads-3.png'
+        },
+        {
+          id: 47,
+          img: '/imgs/ads/ads-4.jpg'
+        }
       ]
     }
   }
@@ -225,6 +251,18 @@ export default {
         height: 100%;
       }
     }
+  }
+  .ads-box {
+    @include flex();
+    margin-top: 14px;
+    margin-bottom: 31px;
+    a {
+      width: 296px;
+      height: 167px;
+    }
+  }
+  .banner {
+    margin-bottom: 50px;
   }
 }
 </style>

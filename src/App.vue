73f3dda362f010b1175005282e92780df17e404c
David <xiaodi.david@gmail.com>
2020-02-07 00:05:42 +1000

    Fix scss issue

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 @import './assets/scss/reset.scss';
-@import './assets/scss/modal.scss';
-@import './assets/scss/mixin.scss';
-@import './assets/scss/config.scss';
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -39,3 +39,8 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+@import './../assets/scss/config.scss';
+@import './../assets/scss/mixin.scss';
+@import './../assets/scss/modal.scss';
+</style>
